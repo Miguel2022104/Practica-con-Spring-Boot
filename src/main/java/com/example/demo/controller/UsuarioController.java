b540254dd61a1d4e70cbcee5e40d520005681a2e
Miguel Enrique Martinez <miguel.enrique@Quipux.local>
2026-02-09 09:12:59 -0500

    Se agregaron comentarios sobre las rutas el consumo de los servicios

--- src/main/java/com/example/demo/controller/UsuarioController.java
+++ src/main/java/com/example/demo/controller/UsuarioController.java
@@ -3,11 +3,14 @@ package com.example.demo.controller;
 import com.example.demo.entity.Usuario;
 import com.example.demo.repository.UsuarioRepository;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
+
+/*Esta es la clase de controlller de Usuario*/
+
+
 @RestController
-@RequestMapping("/api/usuarios")
+@RequestMapping("/api/usuarios") /*Ruta para acceder a los usuarios mediante API*/
 @CrossOrigin(origins = "*")
 public class UsuarioController {
 
@@ -18,16 +21,19 @@ public class UsuarioController {
         this.repository = repository;
     }
 
+    /*Metodo POST para guardar usuarios*/
     @PostMapping
     public Usuario guardar(@RequestBody Usuario usuario) {
         return repository.save(usuario);
     }
 
+    /*Metodo GET para guardar usuarios*/
     @GetMapping
     public List<Usuario> listar() {
         return repository.findAll();
     }
 
+    /*Metodo PUT para guardar usuarios*/
     @PutMapping("/{id}")
     public Usuario actualizar(@PathVariable Long id, @RequestBody Usuario usuario) {
         Usuario existente = repository.findById(id)
@@ -39,6 +45,7 @@ public class UsuarioController {
         return repository.save(existente);
     }
 
+    /*Metodo DELETE para guardar usuarios*/
     @DeleteMapping("/{id}")
     public void eliminar(@PathVariable Long id) {
         repository.deleteById(id);
